ac520bd8de0272f2ee676aa2784cd95436e0576c
 lesech35230 <denis.lesech@gmail.com>
2018-11-11 20:06:38 +0100

    commit

--- private/src/app/app.component.ts
+++ private/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { LoginService } from './services/login.service';
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   isLoggin = false;
- constructor(private lservice: LoginService) {
+ constructor(private lservice: LoginService ,
+   private matIconRegistry: MatIconRegistry ,
+   private domSanitizer: DomSanitizer ) {
+   this.matIconRegistry.addSvgIcon(
+    'swimvalid',
+    this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/swimok.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'swiminvalid',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/swimko.svg')
+      );
+
+
+  
   }
 
   ngOnInit() {
