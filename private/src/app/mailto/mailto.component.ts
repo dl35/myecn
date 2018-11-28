5397b5f896921968d45d5f587ade7cefe8a7d44d
 lesech35230 <denis.lesech@gmail.com>
2018-11-28 16:24:21 +0100

    ok

--- private/src/app/mailto/mailto.component.ts
+++ private/src/app/mailto/mailto.component.ts
@@ -2,7 +2,7 @@ import { MatSnackBar, MatDrawer } from '@angular/material';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
-
+import {QuillModule} from 'ngx-quill';
 @Component({
   selector: 'app-mailto',
   templateUrl: './mailto.component.html',
