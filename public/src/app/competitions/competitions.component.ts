eaf2c77ce87c8fb84541debbc03a32a1c484637d
 lesech35230 <denis.lesech@gmail.com>
2019-01-18 13:04:42 +0100

    ok

--- public/src/app/competitions/competitions.component.ts
+++ public/src/app/competitions/competitions.component.ts
@@ -1,19 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { CompetitionsService, ICompetitions, IEngagements } from './services/competitions.service';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-competitions',
   templateUrl: './competitions.component.html',
   styleUrls: ['./competitions.component.scss']
 })
-export class CompetitionsComponent implements OnInit {
+export class CompetitionsComponent implements OnInit, OnDestroy {
+
 
   dataForm = new FormControl();
+  datas: ICompetitions[];
+  engs: IEngagements[];
+  destroyed$: Subject<any> = new Subject();
+  loading$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-
-  constructor() { }
+  constructor(private cService: CompetitionsService ) { }
 
   ngOnInit() {
+    this.cService.getCompetitions().pipe(takeUntil(this.destroyed$)).subscribe(
+      (datas) => { this.datas = datas; },
+      (error) => { console.log( error ); }
+    );
+  }
+
+  private getEngagements( id ) {
+      this.loading$.next(true);
+      this.cService.getEngagements(id).pipe(takeUntil(this.destroyed$)).subscribe(
+        (engs) => { this.engs = engs; console.log( engs );  this.setLoading(); } ,
+        (error) => { console.log( error ); }
+      );
+  }
+
+  private setLoading() {
+    setTimeout(() => {
+      this.loading$.next(false);
+    }, 500);
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+    this.loading$.unsubscribe();
   }
 
 }
--- public/src/app/competitions/services/competitions.service.ts
+++ public/src/app/competitions/services/competitions.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { shareReplay } from 'rxjs/operators';
+
+
+export interface ICompetitions {
+
+  id: number;
+  label: string;
+}
+
+export interface IEngagements {
+
+  id_licencies?: string;
+  nom: string;
+  prenom: string;
+  categorie: string;
+  sexe: string;
+  rang: string;
+  eng: IEng;
+}
+
+
+interface IEng {
+   day: string ;
+   presence: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompetitionsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private url = '/api/public/competitions';
+
+  public  getCompetitions() {
+     return this.http.get<Array<ICompetitions>>( this.url ).pipe(shareReplay(1));
+  }
+
+  public  getEngagements(id) {
+    const url = this.url + '/' + id ;
+    return this.http.get<Array<IEngagements>>( url ).pipe(shareReplay(1));
+ }
+
 }
