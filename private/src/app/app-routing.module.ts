10f4215c8d784152c29b13dc5b4169121d92e170
 lesech35230 <denis.lesech@gmail.com>
2020-07-16 14:55:42 +0200

    ok

--- private/src/app/app-routing.module.ts
+++ private/src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
+  imports: [RouterModule.forRoot(routes, { useHash: true ,
     preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
